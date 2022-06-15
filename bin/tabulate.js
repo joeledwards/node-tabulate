8e2b727bd3c1e6c47fe1b9ed46b90969d6f05cac
Joel Edwards <joeledwards@gmail.com>
2022-06-15 17:13:00 -0600

    feat: options for field / index selection

--- bin/tabulate.js
+++ bin/tabulate.js
@@ -12,6 +12,17 @@ app({
         'convert a file into a table in the terminal',
         yarg => {
           yarg
+            .option('fields', {
+              coerce: parseList,
+              desc: 'maximum number of rows to output',
+              alias: 'f',
+            })
+            .option('indices', {
+              coerce: parseNumbers,
+              desc: 'maximum number of rows to output',
+              alias: 'i',
+              conflicts: ['fields'],
+            })
             .option('limit', {
               type: 'number',
               desc: 'maximum number of rows to output',
@@ -28,3 +39,39 @@ app({
   await tabulate(args)
 })
 
+// Parse a list of strings
+function parseList (list) {
+  if (typeof list != 'string' && typeof list != 'number') {
+    throw new Error('An argument is required')
+  }
+
+  const parts = list.toString().split(',').map(str => {
+    if (str.trim() === '') {
+      throw new Error('List values may not be empty')
+    }
+
+    return str
+  })
+
+  if (parts.length < 1) {
+    throw new Error('At least 1 value required in list')
+  }
+
+  return parts
+}
+
+// Parse a list of numbers
+function parseNumbers (list) {
+  let hasNan = false
+  const numbers = parseList(list).map(str => {
+    const num = Number(str)
+
+    if (isNaN(num)) {
+      throw new Error('All values must be numbers')
+    }
+
+    return num
+  })
+
+  return numbers
+}
